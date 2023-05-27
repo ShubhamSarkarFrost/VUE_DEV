22f22a857ea82769ca951317d954ed1cba75dffe
Ronnie <roiecse8@gmail.com>
2023-05-27 02:49:45 -0700

    VUE_DEV_ANIMATIONS_COMPLETE_27-05-2023_03:19_PM_IST

---
+++ vue_animations_full_project/src/pages/AllUsers.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        <h2>All Users</h2>
+        <routerLink to="/goals">Course Goals</routerLink>
+    </div>
+</template>
---
+++ vue_animations_full_project/src/pages/CourseGoals.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        <h2>Course Goals</h2>
+        <routerLink to="/">All Users</routerLink>
+    </div>
+</template>
