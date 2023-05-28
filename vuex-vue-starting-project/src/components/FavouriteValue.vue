264b4f8c36567257a9bc655229755946f9490ac5
Shubham Sarkar <ronniecse8@gmail.com>
2023-05-28 22:40:33 +0530

    VUE_DEV_VUEX_Getters_Complete_10:40_PM_IST_28_05_2023

---
+++ vuex-vue-starting-project/src/components/FavouriteValue.vue
@@ -0,0 +1,19 @@
+<template>
+        <h3>{{ counter }}</h3>
+        <p>We Do More!!</p>
+</template>
+
+
+<script>
+export default {
+
+        computed: {
+                counter() {
+                        return this.$store.getters.normalizedCounter;
+                }
+        },
+
+
+}
+
+</script>
